db8abd0f9f7d04c0eda11d08dcca25e9ba083cbe
Ken Gilmer <kengilmer@clearchannel.com>
2014-02-11 23:00:56 +0900

    remove unneeded code from parent project.  minor ui tweaks.

--- statsdtop.go
+++ statsdtop.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"bytes"
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/wsxiaoys/terminal"
 	// "github.com/wsxiaoys/terminal/color"
 	"log"
-	"math"
 	"net"
 	"os"
 	"os/signal"
 	"regexp"
 	"runtime"
-	"sort"
 	"strconv"
 	"strings"
 	"syscall"
@@ -22,7 +19,7 @@ import (
 )
 
 const (
-	VERSION                 = "0.5.2-alpha"
+	VERSION                 = "0.0.1"
 	MAX_UNPROCESSED_PACKETS = 1000
 	MAX_UDP_PACKET_SIZE     = 512
 )
@@ -65,7 +62,6 @@ func (a *Percentiles) String() string {
 
 var (
 	serviceAddress   = flag.String("address", ":8125", "UDP service address")
-	graphiteAddress  = flag.String("graphite", "127.0.0.1:2003", "Graphite service address (or - to disable)")
 	flushInterval    = flag.Int64("flush-interval", 10, "Flush interval (seconds)")
 	debug            = flag.Bool("debug", false, "print statistics sent to graphite")
 	showVersion      = flag.Bool("version", false, "print version string")
@@ -85,20 +81,11 @@ var (
 )
 
 func monitor() {
-	period := time.Duration(*flushInterval) * time.Second
-	ticker := time.NewTicker(period)
 	for {
 		select {
 		case sig := <-signalchan:
 			fmt.Printf("!! Caught signal %d... shutting down\n", sig)
-			if err := submit(time.Now().Add(period)); err != nil {
-				log.Printf("ERROR: %s", err)
-			}
 			return
-		case <-ticker.C:
-			if err := submit(time.Now().Add(period)); err != nil {
-				log.Printf("ERROR: %s", err)
-			}
 		case s := <-In:
 			if s.Modifier == "ms" {
 				_, ok := timers[s.Bucket]
@@ -115,159 +102,9 @@ func monitor() {
 					counters[s.Bucket] = 0
 				}
 				counters[s.Bucket] += int64(float64(s.Value.(int64)) * float64(1/s.Sampling))
-				fmt.Printf("counter: %s %d %d\n", s.Bucket, counters[s.Bucket], time.Now().Unix())
 			}
 		}
 	}
-}
-
-func submit(deadline time.Time) error {
-	var buffer bytes.Buffer
-	var num int64
-
-	now := time.Now().Unix()
-
-	client, err := net.Dial("tcp", *graphiteAddress)
-	if err != nil {
-		if *debug {
-			log.Printf("WARNING: resetting counters when in debug mode")
-			processCounters(&buffer, now)
-			processGauges(&buffer, now)
-			processTimers(&buffer, now, percentThreshold)
-		}
-		errmsg := fmt.Sprintf("dialing %s failed - %s", *graphiteAddress, err)
-		return errors.New(errmsg)
-	}
-	defer client.Close()
-
-	err = client.SetDeadline(deadline)
-	if err != nil {
-		errmsg := fmt.Sprintf("could not set deadline:", err)
-		return errors.New(errmsg)
-	}
-
-	num += processCounters(&buffer, now)
-	num += processGauges(&buffer, now)
-	num += processTimers(&buffer, now, percentThreshold)
-	if num == 0 {
-		return nil
-	}
-
-	if *debug {
-		for _, line := range bytes.Split(buffer.Bytes(), []byte("\n")) {
-			if len(line) == 0 {
-				continue
-			}
-			log.Printf("DEBUG: %s", line)
-		}
-	}
-
-	_, err = client.Write(buffer.Bytes())
-	if err != nil {
-		errmsg := fmt.Sprintf("failed to write stats - %s", err)
-		return errors.New(errmsg)
-	}
-
-	log.Printf("sent %d stats to %s", num, *graphiteAddress)
-
-	return nil
-}
-
-func processCounters(buffer *bytes.Buffer, now int64) int64 {
-	var num int64
-	// continue sending zeros for counters for a short period of time even if we have no new data
-	// note we use the same in-memory value to denote both the actual value of the counter (value >= 0)
-	// as well as how many turns to keep the counter for (value < 0)
-	// for more context see https://github.com/bitly/statsdaemon/pull/8
-	for s, c := range counters {
-		switch {
-		case c <= *persistCountKeys:
-			// consider this purgable
-			delete(counters, s)
-			continue
-		case c < 0:
-			counters[s] -= 1
-			fmt.Fprintf(buffer, "%s %d %d\n", s, 0, now)
-		case c >= 0:
-			counters[s] = -1
-			fmt.Fprintf(buffer, "%s %d %d\n", s, c, now)
-		}
-		num++
-	}
-	return num
-}
-
-func processGauges(buffer *bytes.Buffer, now int64) int64 {
-	var num int64
-	for g, c := range gauges {
-		if c == math.MaxUint64 {
-			continue
-		}
-		fmt.Fprintf(buffer, "%s %d %d\n", g, c, now)
-		gauges[g] = math.MaxUint64
-		num++
-	}
-	return num
-}
-
-func processTimers(buffer *bytes.Buffer, now int64, pctls Percentiles) int64 {
-	var num int64
-	for u, t := range timers {
-		if len(t) > 0 {
-			num++
-
-			sort.Sort(t)
-			min := t[0]
-			max := t[len(t)-1]
-			maxAtThreshold := max
-			count := len(t)
-
-			sum := uint64(0)
-			for _, value := range t {
-				sum += value
-			}
-			mean := float64(sum) / float64(len(t))
-
-			for _, pct := range pctls {
-
-				if len(t) > 1 {
-					var abs float64
-					if pct.float >= 0 {
-						abs = pct.float
-					} else {
-						abs = 100 + pct.float
-					}
-					// poor man's math.Round(x):
-					// math.Floor(x + 0.5)
-					indexOfPerc := int(math.Floor(((abs / 100.0) * float64(count)) + 0.5))
-					if pct.float >= 0 {
-						indexOfPerc -= 1 // index offset=0
-					}
-					maxAtThreshold = t[indexOfPerc]
-				}
-
-				var tmpl string
-				var pctstr string
-				if pct.float >= 0 {
-					tmpl = "%s.upper_%s %d %d\n"
-					pctstr = pct.str
-				} else {
-					tmpl = "%s.lower_%s %d %d\n"
-					pctstr = pct.str[1:]
-				}
-				fmt.Fprintf(buffer, tmpl, u, pctstr, maxAtThreshold, now)
-			}
-
-			var z Uint64Slice
-			timers[u] = z
-
-			fmt.Fprintf(buffer, "%s.mean %f %d\n", u, mean, now)
-			fmt.Fprintf(buffer, "%s.upper %d %d\n", u, max, now)
-			fmt.Fprintf(buffer, "%s.lower %d %d\n", u, min, now)
-			fmt.Fprintf(buffer, "%s.count %d %d\n", u, count, now)
-		}
-	}
-	return num
 }
 
 var packetRegexp = regexp.MustCompile("^([^:]+):(-?[0-9]+)\\|(g|c|ms)(\\|@([0-9\\.]+))?\n?$")
@@ -336,7 +173,6 @@ func udpListener() {
 		}
 
 		for _, p := range parseMessage(message[:n]) {
-			log.Printf("recevied message")
 			In <- p
 		}
 	}
@@ -346,14 +182,17 @@ func top() {
 	for {
 		terminal.Stdout.
 			Clear().
-			Move(0,0).
+			Move(1,0).
+			Color("y").
+			Print(fmt.Sprintf("%v", time.Now().Unix())).
+			Move(2,0).
 			Color("r").
 			Print("Counters").
 			Right(4).
 			Color("b")
 		
 		var x = 4
-		var y = 2
+		var y = 3
 		
 		for s, c := range counters {
 			terminal.Stdout.
